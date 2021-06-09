4a504463bdb0e4f1071c8be4e07d944cfea804f4
anchelsea <nguyenvinhanchelseafc@gmail.com>
2021-06-09 16:22:41 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root 'application#hello'
 
-  resources :articles do
-    resources :comments
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
